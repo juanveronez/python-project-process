bb9cf8612c673975613e715ab59ab0f4d368ba52
Juan Veronez <juan.monteirov@gmail.com>
2025-02-17 10:15:00 -0300

    Creates pyproject task to run fake_data. Update fake_data script to add fake incorrect and correct data load

--- src/utils/fake_sales.py
+++ src/utils/fake_sales.py
@@ -18,19 +18,57 @@ def generate_sale():
         "category": random.choice(list(SaleCategory)),
     }
 
-def generate_sales_list(n: int):
+def generate_invalid_key_value():
+    invalid_sale = {
+        "email": "not-an-email",
+        "date": "00-00-0000",
+        "price": round(random.uniform(-1000.0, -10.0), 2),
+        "product": "",
+        "quantity": random.randint(-100, -1),
+        "category": "invalid-category"
+    }
+
+    sale_keys = invalid_sale.keys()
+    selected_key = list(sale_keys)[random.randint(0, len(sale_keys)-1)]
+    selected_value = invalid_sale[selected_key]
+
+    return selected_key, selected_value
+
+
+
+
+def corrupt_sale(sale: Sale):
+    selected_key, selected_value = generate_invalid_key_value()
+    
+    return {
+        **sale.model_dump(),
+        "category": sale.category.value,
+        selected_key: selected_value
+    }
+
+
+
+def generate_sales_list(n: int, valid: bool = True):
     sales_data: list[dict[str, any]] = []
     for _ in range(n):
         sale_data = generate_sale()
         sale = Sale(**sale_data)
         
-        sales_data.append({ 
-            **sale.model_dump(),
-            "category": sale_data["category"].value
-        })
+        if not valid and random.randint(0, 9) % 2 == 0:
+            sales_data.append(corrupt_sale(sale))
+        else:
+            sales_data.append({ 
+                **sale.model_dump(),
+                "category": sale.category.value
+            })
     return sales_data
     
+def create_file(file_name: str, valid: bool = True):
+    df = pd.DataFrame(generate_sales_list(20, valid))
+    df.to_excel(f"data/{file_name}.xlsx", index=False)
+    print(f"Excel file '{file_name}.xlsx' created successfully.")
+
+
 if __name__ == "__main__":
-    df = pd.DataFrame(generate_sales_list(10))
-    df.to_excel("invalid_sales_data.xlsx", index=False)
-    print("Excel file 'invalid_sales_data.xlsx' created successfully.")
+    create_file("sales_data")
+    create_file("invalid_sales_data", valid=False)
